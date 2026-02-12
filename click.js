465c3d135ca1a32a474155b404bd637553649f6f
siva123web <sivapatti68@gmail.com>
2026-02-12 13:34:37 -0500

    Update click.js

--- click.js
+++ click.js
@@ -36,9 +36,9 @@ function showMarriage() {
     </p>" +
 
     "<div class='futureImages fadeIn'>\
-        <img src='https://source.unsplash.com/600x400/?happy-family' alt='Family'>\
-        <img src='https://source.unsplash.com/600x400/?luxury-car' alt='Car'>\
-        <img src='https://source.unsplash.com/600x400/?beautiful-house' alt='House'>\
+        <img src='https://source.unsplash.com/600x400/?happy-family'>\
+        <img src='https://source.unsplash.com/600x400/?luxury-car'>\
+        <img src='https://source.unsplash.com/600x400/?beautiful-house'>\
     </div>";
 }
 
